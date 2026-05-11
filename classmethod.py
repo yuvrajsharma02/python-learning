a10fa99a0b67b0b862266ca5fd4222a5140cfb9a
yuvrajsharma.02 <yuvrajsharma.hsc@gmail.com>
2026-05-11 12:56:37 +0530

    learn class method

--- classmethod.py
+++ classmethod.py
@@ -1,1 +1,38 @@
 # class method = allow operation related to the class itself
+#  take class as the first parameter , which represent the class itself
+
+class Student:
+
+    count = 0
+    total_gpa = 0
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1
+        Student.total_gpa += gpa
+    #INSTANCE METHOD
+
+    def get_info(self):
+        return f"{self.name} {self.gpa}"
+    
+    @classmethod
+
+    def get_count(cls):
+        return f"Total # of student: {cls.count}"
+    
+    @classmethod
+
+    def get_average_gpa(cls):
+        if cls.count ==0:
+            return 0
+        else:
+            return f"{cls.total_gpa / cls.count}"
+    
+student1 = Student("Spongbob", 3.2)
+student2 = Student("patrick", 2.0)
+student3 = Student("sandy", 4.0)
+
+print(Student.get_count())
+print(Student.get_average_gpa())
+
